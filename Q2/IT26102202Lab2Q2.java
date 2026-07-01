67e32f95b622a552eaba8868d5a4f5ebc7de07dd
IT26102202 <kaweendya@gmail.com>
2026-07-01 10:51:23 +0530

    Add files via upload

---
+++ Q2/IT26102202Lab2Q2.java
@@ -0,0 +1,14 @@
+public class IT26101529Lab2Q2 {
+	
+            public static void main(String[] args)
+		{
+        
+           double length = 10;         
+           double PI = 3.14;   
+		   double squareperimeter = 4 * length;
+           double radius = squareperimeter / (2 * PI);
+        
+        System.out.println("Radius of the circular fence:"+radius);
+        }
+}
+
