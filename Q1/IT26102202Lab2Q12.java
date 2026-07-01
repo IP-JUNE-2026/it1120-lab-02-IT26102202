ac34117716c9bca800567ce723e071e392803d6d
IT26102202 <kaweendya@gmail.com>
2026-07-01 10:50:17 +0530

    Add files via upload

---
+++ Q1/IT26102202Lab2Q12.java
@@ -0,0 +1,12 @@
+public class IT26101441Lab2Q1 {
+
+        public static void main (String [] args)
+		{ 
+		   double perimeter = 100;
+		   double length = perimeter/(2*(1+3.0/4));
+		   double width = length*(3.0/4);
+		   
+		   System.out.println("length of the fence:"+length);
+		   System.out.println("width of the fence:"+width);
+		}
+}
