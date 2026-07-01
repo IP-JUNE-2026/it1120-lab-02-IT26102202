85b416505b19ccf3fdfb50d5b3231aac4b8c0e76
IT26102202 <kaweendya@gmail.com>
2026-07-01 10:52:39 +0530

    Add files via upload

---
+++ Q3/IT26102202Lab2Q3.java
@@ -0,0 +1,10 @@
+public class IT26101441Lab2Q3 {
+
+       public static void main (String [] args)
+	   {
+	       double sideA = 3;
+		   double sideB = 4;
+		   double hypotenus = Math.sqrt (sideA * sideA + sideB * sideB);
+		   System.out.println ("Length of the hypotenus="+hypotenus);
+	   }
+}
